414c043df110a3d2ad91df83b7a030913bbd07df
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-03-26 14:56:21 +0600

    partially done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,17 +10,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProfileController extends Controller
 {
-//     public function CreateProfile(Request $request):JsonResponse{
-//         $user_id=$request->header('id');
-//         $request->merge(['user_id'=>$user_id]);
-//         $data=CustomerProfile::updateOrCreate(['user_id'=>$user_id],$request->input());
-//  return ResponseHelper::Out('success',$data,200);
-//     }
+    public function CreateProfile(Request $request):JsonResponse{
+        $user_id=$request->header('id');
+        $request->merge(['user_id'=>$user_id]);
+        $data=CustomerProfile::updateOrCreate(['user_id'=>$user_id],$request->input());
+ return ResponseHelper::Out('success',$data,200);
+    }
 
-//     public function ReadProfile(Request $request):JsonResponse{
-//         $user_id=$request->header('id');
-//         $data=CustomerProfile::where('user_id',$user_id)->with('user')->first();
-//         return  ResponseHelper::Out('success',$data,200);
-//     }
+    public function ReadProfile(Request $request):JsonResponse{
+        $user_id=$request->header('id');
+        $data=CustomerProfile::where('user_id',$user_id)->with('user')->first();
+        return  ResponseHelper::Out('success',$data,200);
+    }
  }
 
