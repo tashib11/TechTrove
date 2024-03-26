0d218aec3b8960a9ec214ed8d41eb66751f96e4b
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-03-26 23:08:20 +0600

    carlist controlling done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Helper\ResponseHelper;
 use App\Models\CustomerProfile;
 use App\Models\Product;
+use App\Models\ProductCart;
 use App\Models\ProductDetails;
 use App\Models\ProductReview;
 use App\Models\ProductWish;
@@ -79,4 +80,46 @@ class ProductController extends Controller
          $data=ProductWish::where(['user_id'=>$user_id,'product_id'=>$request->product_id])->delete();
          return responseHelper::Out('success',$data,200);
         }
+
+        public function CreateCartList(Request $request):JsonResponse{
+            $user_id=$request->header('id');
+            $product_id=$request->product_id;
+            $color=$request->input('color');
+            $size=$request->input('size');
+            $qty=$request->input('qty');
+
+            $unitPrice=0;
+
+            $productDetails=Product::where('id','=',$product_id)->first();
+            if($productDetails->discount> 0){
+                $unitPrice=$productDetails->discount_proce;
+            }else{
+                $unitPrice=$productDetails->price;
+            }
+            $totalPrice=$qty*$unitPrice;
+
+            $data=ProductCart::updateOrCreate(['user_id'=>$user_id,'product_id'=>$product_id],[
+                'user_id'=>$user_id,
+                'product_id'=>$product_id,
+                'color'=>$color,
+                'size'=>$size,
+                'qty'=>$qty,
+                'price'=>$totalPrice
+            ]
+            );
+            return responseHelper::Out('success',$data,200);
+        }
+
+        public function CartList(Request $request):JsonResponse{
+             $user_id=$request->header('id');
+             $data=ProductCart::where('user_id',$user_id)->with('product')->get();
+        return responseHelper::Out('success',$data,200);
+        }
+
+        public function DeleteCartList(Request $request):JsonResponse{
+            $user_id=$request->header('id');
+            $data=ProductCart::where(['user_id'=>$user_id,'product_id'=>$request->product_id])->delete();
+   return responseHelper::Out('success',$data,200);
+        }
+
 }
