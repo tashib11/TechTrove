3e97fdecb4eebba7352743e094bcdc78fe4e0795
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-04-27 15:02:36 +0600

    error solved

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="Shopwise is Powerful features and You Can Use The Perfect Build this Template For Any eCommerce Website. The template is built for sell Fashion Products, Shoes, Bags, Cosmetics, Clothes, Sunglasses, Furniture, Kids Products, Electronics, Stationery Products and Sporting Goods.">
     <meta name="keywords" content="ecommerce, electronics store, Fashion store, furniture store,  bootstrap 4, clean, minimal, modern, online store, responsive, retail, shopping, ecommerce store">
 
-    <title>Apple Shop - eCommerce</title>
+    <title>TechTrove - eCommerce</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.png')}}">
 
     <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
