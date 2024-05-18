140834928f67cd0bae1d06cd7561f10622fd412e
tashib11 <islam2007011@stud.kuet.ac.bd>
2024-05-18 23:09:14 +0600

    category admin done

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,9 +3,25 @@ namespace App\Http\Controllers;
 use App\Helper\ResponseHelper;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    public function create(){
+        return view('admin.products.category');
+    }
+
+    public function store(Request $request) {
+        $category = Category::create($request->all() );
+       // return $request->all();
+       if($category) {
+           return redirect()->route('category.create')->with('success', 'category created successfully');
+       }else {
+           return redirect()->route('category.create')->with('error', 'category creation failed');
+       }
+   }
+
     public function ByCategoryPage()
     {
         return view('pages.product-by-category');
