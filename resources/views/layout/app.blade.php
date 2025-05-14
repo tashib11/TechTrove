373f6e368c320e36c056ec92fbdd95802f1c4ebc
tashib11 <islam2007011@stud.kuet.ac.bd>
2025-05-14 21:53:59 +0600

    zoom feature updated

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -41,7 +41,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+    
+    {{-- zoom feature script --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/elevatezoom/3.0.8/jquery.elevatezoom.min.js"></script>
 
     <script src="{{ asset('assets/js/axios.min.js') }}"></script>
