53b2f81d71a520665ac9a1e3576b999908380430
tashib11 <islam2007011@stud.kuet.ac.bd>
2025-05-14 15:30:02 +0600

    product details update -select and edit page updated

--- resources/views/admin/products/detailedit.blade.php
+++ resources/views/admin/products/detailedit.blade.php
@@ -5,109 +5,78 @@
     <div class="container-fluid my-2">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Make Details of Product</h1>
+                <h1>Edit Product Detail</h1>
             </div>
             <div class="col-sm-6 text-right">
-                <a href="{{ asset ('/') }}" class="btn btn-primary">Home</a>
+                <a href="{{ asset('/') }}" class="btn btn-primary">Home</a>
             </div>
         </div>
     </div>
-    <!-- /.container-fluid -->
 </section>
-<!-- Main content -->
+
 <section class="content">
-    <!-- Default box -->
-    <form action="{{ route("product.detail.update", $products->id) }}" method="POST" name="productForm" id="productForm">
+    <form action="{{ route('product.detail.update', $products->id) }}" method="POST" name="productForm" id="productForm" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8">
+                    <!-- Description -->
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <label for="des">Description</label>
+                            <textarea name="des" id="des" class="summernote">{{ $products->des }}</textarea>
+                        </div>
+                    </div>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="row">
+                    <!-- Images -->
+                    @for ($i = 1; $i <= 4; $i++)
+                        @php
+                            $imgField = "img{$i}";
+                            $imgUrl = $products->$imgField;
+                        @endphp
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <label for="img{{ $i }}">Image {{ $i }}</label>
+                                <input type="file" name="img{{ $i }}" id="img{{ $i }}" class="form-control">
+                                <p class="error"></p>
 
-                            <div class="col-md-12">
-                                <div class="mb-3">
-                                    <label for="description">Description</label>
-                                    <textarea name="des" id="des" cols="30" rows="10" class="summernote" placeholder="Description">{{ $products->des  }}</textarea>
+                                <div id="imagePreviewCard{{ $i }}" class="card mt-3 {{ $imgUrl ? '' : 'd-none' }}">
+                                    <img id="imagePreview{{ $i }}" src="{{ $imgUrl }}" class="card-img-top" style="max-height: 200px; object-fit: contain;">
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label for="alt{{ $i }}">Image Alt Text</label>
+                                            <input type="text" class="form-control" name="alt{{ $i }}" placeholder="Describe the image">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="width{{ $i }}">Set Width (px)</label>
+                                            <input type="number" class="form-control" name="width{{ $i }}" placeholder="e.g., 200">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="height{{ $i }}">Set Height (px)</label>
+                                            <input type="number" class="form-control" name="height{{ $i }}" placeholder="e.g., 300">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="image">Image1 </label>
-                                <input type="text" name="img1" id="img1" class="form-control" placeholder="image link" value="{{ $products->img1}}">
-                                <p class="error"></p>
-                            </div>
+                    @endfor
+
+                    <!-- Color and Size -->
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <label for="color">Color</label>
+                            <input type="text" name="color" id="color" class="form-control" value="{{ $products->color }}" placeholder="e.g., red, green">
+                        </div>
+                        <div class="card-body">
+                            <label for="size">Size</label>
+                            <input type="text" name="size" id="size" class="form-control" value="{{ $products->size }}" placeholder="e.g., 16, 17">
                         </div>
                     </div>
                 </div>
 
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="image">Image2 </label>
-                                <input type="text" name="img2" id="img2" class="form-control" placeholder="image link" value="{{ $products->img2}}">
-                                <p class="error"></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <!-- Sidebar -->
 
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="image">Image3 </label>
-                                <input type="text" name="img3" id="img3" class="form-control" placeholder="image link" value="{{ $products->img3}}">
-                                <p class="error"></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="image">Image4 </label>
-                                <input type="text" name="img4" id="img4" class="form-control" placeholder="image link" value="{{ $products->img4}}">
-                                <p class="error"></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="color">Color </label>
-                                <input type="text" name="color" id="color" class="form-control"  value="{{ $products->color}}">
-                                <p class="error"></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="col-md-12">
-                            <div class="mb-3">
-                                <label for="size">size </label>
-                                <input type="text" name="size" id="size" class="form-control"  value="{{ $products->size}}">
-                                <p class="error"></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
-            </div>
             <div class="col-md-4">
 
                 <div class="card">
@@ -126,65 +95,68 @@
                                 @endif
 
                             </select>
-                            <p class="error"> </p>
-                        </div>
 
+                        </div>
                     </div>
                 </div>
+            </div>
 
-
+            <div class="pb-5 pt-3">
+                <button type="submit" class="btn btn-success">Update</button>
+                <a href="{{ asset('/Dashboard/ProductCreate') }}" class="btn btn-outline-dark ml-3">Cancel</a>
             </div>
         </div>
-
-        <div class="pb-5 pt-3">
-            <button type="submit" class="btn btn-primary">Update</button>
-            <a href="{{ asset('/Dashboard/DetailsSelect') }}" class="btn btn-outline-dark ml-3">Cancel</a>
-        </div>
-    </div>
-</form>
-    <!-- /.card -->
+    </form>
 </section>
 @endsection
 
-
-
-@section('customJs')
+@section('script')
 <script>
+$(document).ready(function () {
+    $('#product').select2({
+        placeholder: 'Select a Product',
+        allowClear: true,
+        width: '100%'
+    });
 
-$("#productForm").submit(function(event){
-    event.preventDefault();
-       var formArray = $(this).serializeArray();
-    $.ajax({
-        url:'{{ route("product.detail.update", $products->id) }}',
-        type:'post',
-        data:formArray,
-        dataType: 'json',
-        success:function(response){
-            if(response['status']== true){
-            console.log(response);
-            }else{
+    $('.summernote').summernote();
 
-                var error = response['errors'];
+    // Set up all previews
+    for (let i = 1; i <= 4; i++) {
+        setupImagePreview(`img${i}`, `imagePreviewCard${i}`, `imagePreview${i}`);
+    }
 
-            //  if(error['title']){
-            //         $('#title').addClass('is-invalid').siblings('p').
-            //         addClass('invalid-feedback').html(error['title']);
-            //     }else{
-            //         $('#title').removeClass('is-invalid').siblings('p').
-            //         removeClass('invalid-feedback').html("");;
-            //     }
-            $(".error").removeClass('is-invalid').html("");
-            $("input[type=text],select").removeClass('is-invalid');
-            $.each(error,function(key,value){
-                $('#'+key).addClass('is-invalid').siblings('p').
-                addClass('invalid-feedback').html(value);
-            });
+    $("#productForm").submit(function(event){
+        event.preventDefault();
+        var formData = new FormData(this);
 
+        $.ajax({
+            url: $(this).attr('action'),
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            success: function(response){
+                if (response.status == true || response.status === "true") {
+                    window.location.href = "/Dashboard/ProductCreate";
+                } else {
+                    alert("Update failed.");
+                }
+            },
+            error: function(xhr){
+                let res = xhr.responseJSON;
+                if (res && res.errors) {
+                    $(".error").removeClass('is-invalid').html("");
+                    $("input, select").removeClass('is-invalid');
+                    $.each(res.errors, function(key, value){
+                        $('#' + key).addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(value);
+                    });
+                } else {
+                    alert("Something went wrong. Please check your input.");
+                }
             }
-        },
-        error:function(){
-            console.log("something went wrong");
-        }
+        });
     });
 });
 
